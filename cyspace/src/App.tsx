402f4ec6018b92fdabc29e6fdb5e63f66870e165
Fiona Tan <107552356+ftjy@users.noreply.github.com>
2024-11-01 20:08:59 +0800

    added MUI

--- cyspace/src/App.tsx
+++ cyspace/src/App.tsx
@@ -1,72 +1,120 @@
 import React, { useState, useEffect }  from 'react';
 import './App.css';
 import axios from 'axios';
-export default App;
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid2';
+import { styled } from '@mui/material/styles';
 
-// const getAllArticles = () => {
-//   axios.get('http://localhost:3000/api/articles').then((response) => {
-//     setData(response.data);
-//     console.log(data);
-//   })
-// }
+interface ArticleResponse {
+  count: number;
+  next: string;
+  previous:  string;
+  results: Article[];
+}
 
-// const getArticleById = () => {
-//   axios.get('http://localhost:3000/api/articles/:id').then((data) => {
-//     console.log(data)
+interface Article {
+  id: number;
+  title: string;
+  url: string;
+  image_url: string;
+  news_site: string;
+  summary: String;
+  published_at: Date;
+  updated_at: Date;
+  launches: [];
+  events: [];
+}
+
+async function fetchArticles(): Promise<ArticleResponse> {
+  const response = await fetch('http://localhost:3000/');
+  console.log(response)
+  const data = await response.json();
+  return data;
+}
+
+
+// async function getArticleById = () => {
+//   axios.get('http://localhost:3000/api/articles/:id').then((response) => {
+//     console.log(response.data);
+//     return response.data;
 //   });
 // }
 
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+  ...theme.applyStyles('dark', {
+    backgroundColor: '#1A2027',
+  }),
+}));
+
+
 function App() {
-  const [data, setData] = useState([])
-
-  const getAllArticles = () => {
-    axios.get('http://localhost:3000/').then((response) => {
-      setData(response.data);
-      console.log(data);
-    })
-  }
-
-  const getArticleById = () => {
-    axios.get('http://localhost:3000/api/articles/:id').then((data) => {
-      console.log(data)
-    });
-  }
+const [articleResponse, setArticleResponse] = useState<ArticleResponse>();
+const [error, setError] = useState<string | null>(null);
+const [data, setData] = useState([])
 
   useEffect(() => {
-    getAllArticles();
+    async function fetchData() {
+      try {
+        const data = await fetchArticles();
+        console.log(data);
+        setArticleResponse(articleResponse);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchData();
   }, []);
 
-  // return (
-  //   <div className="App">
-  //     <h1 style={{ color: "green" }}>using Axios Library to Fetch Data</h1>
-  //     <center>
-  //       {[data].map((dataObj, index) => {
-  //         return (
-  //           <div
-  //             style={{
-  //               width: "15em",
-  //               backgroundColor: "#CD8FFD",
-  //               padding: 2,
-  //               borderRadius: 10,
-  //               marginBlock: 10,
-  //             }}
-  //           >
-  //             <p style={{ fontSize: 20, color: 'white' }}>{dataObj}</p>
-  //           </div>
-  //         );
-  //       })}
-  //     </center>
-  //   </div>
-  // );
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <button onClick={getAllArticles}>Get All Articles</button>
-        <button onClick={getArticleById}>Get Article By Id</button>
-      </header>
+    <div>
+      {articleResponse?.results?.map((article) => (
+        <div key={article.id}>
+          <Box sx={{ flexGrow: 2 }}>
+            <Grid container spacing={2}>
+              <Grid size={{ xs: 6, md: 8 }}>
+                <Item>
+                  <Card variant="outlined" sx={{ margin: 10}}>
+                    <CardContent>
+                      <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
+                        {article.title}
+                      </Typography>
+                      <Typography variant="h5" component="div">
+                        {article.summary}
+                      </Typography>
+                      <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>adjective</Typography>
+                      <Typography variant="body2">
+                        <a href={article.url}>{article.url}</a>
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small">Learn More</Button>
+                    </CardActions>
+                  </Card>
+                </Item>
+              </Grid>
+            </Grid>
+          </Box>
+        </div>
+      ))}
     </div>
   );
 }
 
+
+export default App;
 
